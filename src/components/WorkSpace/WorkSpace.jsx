9d73e41e4894cf0162c07ea6e3931a8635ffd44c
Ivan Malenko <ivan.malenko18@gmail.com>
2023-05-10 13:22:47 +0300

    add edit button

--- src/components/WorkSpace/WorkSpace.jsx
+++ src/components/WorkSpace/WorkSpace.jsx
@@ -1,16 +1,28 @@
 import { NotesContext } from "context";
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 
 export const WorkSpace = () => {
 
 
     const {notes, selectedNote} = useContext(NotesContext);
+    const [toggleButton, setToggleButton] = useState(true);
+    const textAreaRef = useRef(null);
+    
 
    
+    
+
+   const handleClick = () => {
+  setToggleButton(!toggleButton);
+       if (!toggleButton) {
+      console.log(textAreaRef.current.focus())
+    textAreaRef.current.focus();
+  }
+};
 
     let noteSelectedByUser;
     if (selectedNote) {
-        [noteSelectedByUser] = notes.filter(note => note.id === selectedNote)
+        [noteSelectedByUser] = notes.filter(note => note.id === selectedNote);
     }
 
 
@@ -19,13 +31,16 @@ export const WorkSpace = () => {
     // console.log(notes);
     return (<>
         
-        <textarea
-            
-            onChange={handleChange()}
+        <div>
+            <button onClick={handleClick} disabled={!selectedNote}>Edit</button>
+            <textarea
+                ref={textAreaRef}
+            disabled={toggleButton}
+            onChange={handleChange}
         defaultValue={ selectedNote === null ? `` : noteSelectedByUser.values.ddQ8kFps1ff4owWPldSCow}
         >
             
         </textarea>
-
+        </div>
     </>)
 };
